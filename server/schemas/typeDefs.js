50716c226d06d3617e4b296b26777ca78f81ea9e
JacMcQ <jacqlyn.mcquade@icloud.com>
2024-10-07 22:26:59 -0600

    updated server.js to trouble shoot verfied running on port

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,34 +1,40 @@
-type Book {
-  bookId: ID!
-  authors: [String]
-  title: String!
-  description: String
-  image: String
-  link: String
-}
+const { gql } = require("apollo-server-express");
 
-type Query {
-  getBooks(searchTerm: String!): [Book]
-}
+const typeDefs = gql`
+  type Book {
+    bookId: ID!
+    title: String!
+    authors: [String]
+    description: String
+    image: String
+    link: String
+  }
 
-type Mutation {
-  saveBook(book: BookInput!): User
-  removeBook(bookId: ID!): User
-}
+  input BookInput {
+    bookId: String
+    authors: [String]
+    title: String
+    description: String
+    image: String
+    link: String
+  }
 
-input BookInput {
-  bookId: String
-  authors: [String]
-  title: String
-  description: String
-  image: String
-  link: String
-}
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
 
-type User {
-  _id: ID
-  username: String
-  email: String
-  bookCount: Int
-  savedBooks: [Book]
-}
+  type Query {
+    getBooks(searchTerm: String!): [Book]
+  }
+
+  type Mutation {
+    saveBook(book: BookInput!): User
+    removeBook(bookId: ID!): User
+  }
+`;
+
+module.exports = typeDefs;
