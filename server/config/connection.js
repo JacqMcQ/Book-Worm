fd0e536d146eff1389a7019b8928737371231afd
JacMcQ <jacqlyn.mcquade@icloud.com>
2024-10-10 17:23:54 -0600

    updated credetials for accessing db

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 mongoose.connect(
   process.env.MONGODB_URI ||
-    "mongodb+srv://jacqlynmcquade:<db_password>@bookworm.ohw3w.mongodb.net/?retryWrites=true&w=majority&appName=BookWorm"
+    "mongodb+srv://jacqlynmcquade:Yi0Y5eKcQK70amKl@bookworm.ohw3w.mongodb.net/?retryWrites=true&w=majority&appName=BookWorm"
 );
 
 module.exports = mongoose.connection;
