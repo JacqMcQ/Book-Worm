bd08a4c49944bc7f82b6027e891aba48acba71ca
JacMcQ <jacqlyn.mcquade@icloud.com>
2024-10-10 19:16:54 -0600

    App.jsx updated for wrong page error

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,39 @@
 import "./App.css";
 import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import { ApolloProvider, InMemoryCache, ApolloClient } from "@apollo/client";
 
-// Set up the Apollo Client
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("id_token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
 const client = new ApolloClient({
-  uri: "/graphql", // endpoint for server
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
-  headers: {
-    authorization: localStorage.getItem("id_token")
-      ? `Bearer ${localStorage.getItem("id_token")}`
-      : "",
-  },
 });
 
 function App() {
   return (
     <ApolloProvider client={client}>
-      <>
-        <Navbar />
-        <Outlet />
-      </>
+      <Navbar />
+      <Outlet />
     </ApolloProvider>
   );
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,28 +1,29 @@
-import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-import App from './App.jsx'
-import SearchBooks from './pages/SearchBooks'
-import SavedBooks from './pages/SavedBooks'
+import App from './App.jsx';
+import SearchBooks from './pages/SearchBooks';
+import SavedBooks from './pages/SavedBooks';
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: '*', // Change this to '*' to catch all routes
     element: <App />,
     errorElement: <h1 className='display-2'>Wrong page!</h1>,
     children: [
       {
         index: true,
         element: <SearchBooks />
-      }, {
-        path: '/saved',
+      },
+      {
+        path: 'saved', // Remove the leading slash
         element: <SavedBooks />
       }
     ]
   }
-])
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <RouterProvider router={router} />
-)
+);
